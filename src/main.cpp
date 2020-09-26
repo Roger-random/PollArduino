fc0426954c093fc9fd187c3332391341310eed47
Roger Cheng <roger.random@outlook.com>
2020-09-25 20:16:17 -0700

    Quick test stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,59 @@
 #include <Arduino.h>
 
+#define ADC_RESOLUTION 0.0049 // 4.9 millivolts per unit
+
+int index;
+double voltage;
+char LF = '\n';
+String inString;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    inString = Serial.readStringUntil(LF);
+    if (inString.equals("HBT\r"))
+    {
+      Serial.println("HBT OK");
+    }
+    else if (inString.equals("VER\r"))
+    {
+      Serial.println("VER TESTSTUB 1.0");
+    }
+    else if (inString.equals("RST\r"))
+    {
+      Serial.println("RST OK");
+    }
+    else if (inString.startsWith("MVC"))
+    {
+      // NOTE: pin specifiers are ignored, not checked.
+      index = inString.length()-1;
+      if (inString[index] == '\r')
+      {
+        inString[index] = ' '; // Replace CR with space
+        voltage = analogRead(A1) * ADC_RESOLUTION;
+        inString += voltage;
+        inString += " ";
+        voltage = analogRead(A2) * ADC_RESOLUTION;
+        inString += voltage;
+        inString += " ";
+        voltage = analogRead(A3) * ADC_RESOLUTION;
+        inString += voltage;
+
+        Serial.println(inString);
+      }
+      else
+      {
+        Serial.println("BCM " + inString );
+      }
+    }
+    else
+    {
+      Serial.println("BCM " + inString );
+    }
+  }
 }
